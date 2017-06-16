83680a32c82aadd1efaea99e8f1c0cbdf774be0b
Nikita Myshenkov <nikmyshenkov@gmail.com>
2017-06-16 12:40:37 +0300

    SubstitutionException.h

---
+++ SubstitutionException.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <exception>
+class SubstitutionException: public std::exception
+{
+public:
+	enum ErrorType {Uncr_Line,Uncr_Table,Uncr_Encr,Uncr_Shift,Uncr_Shifr};
+private:
+	ErrorType t;
+public:
+	SubstitutionException(ErrorType T):t(T) {};
+	const char* what();
+};
